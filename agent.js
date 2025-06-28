f2f1768dc978be01c5b2e72c2d49fc93d57dfc4a
Akshatt10 <akshattyagi1010@gmail.com>
2025-06-28 09:49:35 +0530

    assistant_response and tool calling

--- agent.js
+++ agent.js
@@ -3,19 +3,22 @@ import Groq from "groq-sdk";
 const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
 
 async function callAgent(){
-    const completion = await groq.chat.completions
-    .create({
-      messages: [
-        {
+
+    const messages = [ {
             role: "system",
             content: `You are a personal finance AI agent. You help users manage their finances, track expenses, and provide budgeting advice. You are friendly, knowledgeable, and always ready to assist with financial queries.
             current date ${new Date().toUTCString().split('T')[0]}.`,
-        },
-        {
+        }]
+
+        messages.push( {
           role: "user",
           content: "How much money did I spend this month?",
-        },
-      ],
+        })
+
+
+    const completion = await groq.chat.completions
+    .create({
+      messages: messages,
       model: "llama-3.3-70b-versatile",
       tools:[
         {
@@ -44,6 +47,8 @@ async function callAgent(){
 
     console.log(JSON.stringify(completion.choices[0], null, 2));
 
+    messages.push(completion.choices[0].message);
+
     const toolCalls = completion.choices[0].message.tool_calls
     if (!toolCalls){
         console.log(`Assitant response: ${completion.choices[0].message.content}`);
@@ -58,13 +63,50 @@ async function callAgent(){
         if(functionName === 'getTotalExpenses'){
             result = getTotalExpenses(JSON.parse(functionArgs));
         }
+
+    messages.push({
+            role: "tool",
+            content: result,
+            tool_call_id: tool.id,
+    })
+
+    const completion2 = await groq.chat.completions
+    .create({
+      messages: messages, 
+      model: "llama-3.3-70b-versatile",
+      tools:[
+        {
+            type: "function",
+            function:{
+                name: "getTotalExpenses",
+                description: "Get the total expenses between two dates.",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        from: {
+                            type: "string",
+                            description: "The start date in YYYY-MM-DD format."
+                        },
+                        to: {
+                            type: "string",
+                            description: "The end date in YYYY-MM-DD format."
+                        }
+                    },
+                   
+                }
+            }
+        }
+      ]
+    })
+
+
+    console.log(JSON.stringify(completion2.choices[0], null, 2));
+
+
     }
 }
 
 callAgent()
-
-
-
 
 // TOOL making
 
@@ -72,5 +114,5 @@ callAgent()
 function getTotalExpenses(from, to) {
     console.log("Calling the getTotalExpenses tool with parameters:");
     
-    return 199990;
+    return "199990";
 }
